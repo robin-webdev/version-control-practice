ab7aca539b21753138fd49723daae8bc06cf135a
robin-webdev <robin.webdev.me@gmail.com>
2025-10-27 06:22:03 +0200

    feat : Added functionlity to mark a todo as completed by adding relevent styles and sound

--- Components/Main.jsx
+++ Components/Main.jsx
@@ -4,17 +4,52 @@ import { data } from "../src/Context";
 
 const Main = () => {
   const [tasks, setTasks] = useContext(data);
+  const audio = useRef();
+
+  const handleComplete = (ind) => {
+    setTasks(
+      tasks.map((task, index) => {
+        return index !== ind ? task : { ...task, isCompleted: true };
+      })
+    );
+    audio.current.play();
+  };
 
   return (
     <main>
+      <audio ref={audio} src="/todo-list/sound.mp3" id="sound"></audio>
       {tasks.length !== 0 &&
         tasks.map((task, index) => {
           return (
             <div className="task-box" key={index}>
               <div className="task-cont">
-                <div className="checkbox"></div>
-                <div className="task">{task.body}</div>
-                <div className="nocut"></div>
+                <div
+                  className={
+                    !task.isCompleted ? "hover checkbox " : " checkbox newCheck"
+                  }
+                  onClick={() => {
+                    if (!task.isCompleted) {
+                      handleComplete(index);
+                    }
+                  }}
+                >
+                  {" "}
+                  {task.isCompleted && <i className="fa-solid fa-check"></i>}
+                </div>
+                <div
+                  className="task"
+                  style={
+                    task.isCompleted
+                      ? {
+                          color: "rgba(255, 255, 255, 0.6)",
+                          textDecoration: "line-through",
+                        }
+                      : {}
+                  }
+                >
+                  {task.body}
+                </div>
+                <div className="nocut">{task.isCompleted && "Completed"}</div>
               </div>
               <div className="delete-button">
                 <i className="fa-solid fa-trash"></i>
